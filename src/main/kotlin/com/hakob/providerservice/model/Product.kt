6c442dbe903efe1bf735fb8917353d4572e6bee2
hakob21 <hakob21@gmail.com>
2023-08-21 02:26:31 +0200

    add new parameter to response payload body

--- src/main/kotlin/com/hakob/providerservice/model/Product.kt
+++ src/main/kotlin/com/hakob/providerservice/model/Product.kt
@@ -1,5 +1,7 @@
 package com.hakob.providerservice.model
 
 data class Product(
-    val productName: String
+    val productName: String,
+    val productType: String = "productType1",
+    val productCategory: String = "productCategory1",
 )
