9b25077316d2088da9d4977a1bf4d018dc78713f
hakob21 <hakob21@gmail.com>
2023-08-24 02:45:35 +0200

    add a new parameter that consumer expects

--- src/main/kotlin/com/hakob/providerservice/model/Product.kt
+++ src/main/kotlin/com/hakob/providerservice/model/Product.kt
@@ -4,4 +4,5 @@ data class Product(
     val productName: String,
     val productType: String = "productType1",
     val productCategory: String = "productCategory1",
+    val smthnew: String = "productType1",
 )
