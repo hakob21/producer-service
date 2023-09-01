194275d53b731f37428bada4122ba787467c71e2
hakob21 <hakob21@gmail.com>
2023-09-01 08:12:40 +0200

    add expected param

--- src/main/kotlin/com/hakob/providerservice/model/Product.kt
+++ src/main/kotlin/com/hakob/providerservice/model/Product.kt
@@ -4,4 +4,5 @@ data class Product(
     val productName: String = "product1",
     val productType: String = "productType1",
     val productCategory: String = "productCategory1",
+    val newField1: String = "newField1",
 )
